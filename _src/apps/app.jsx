6ea654764cdd437d7e3ef1233977248f72306da3
B. Cedric Cogell <cedric.cogell@gmail.com>
2015-02-14 17:25:55 -0500

    cleaning house before getting to work

---
+++ _src/apps/app.jsx
@@ -0,0 +1,16 @@
+var React = require('react');
+
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        Hello, world! I am a CommentBox.
+      </div>
+    );
+  }
+});
+
+React.render(
+  <CommentBox />,
+  document.getElementById('content')
+);
--- _src/apps/mvi-example/utils/binder.ts
+++
@@ -1,7 +0,0 @@
-function binderer (model, view, intent) {
-  if (view) { view.observe(model); }
-  if (intent) { intent.observe(view); }
-  if (model) { model.observe(intent); }
-}
-
-export = binderer;
