a4d1b907f9b710912ed9cae26ef79d3233d8a097
B. Cedric Cogell <cedric.cogell@gmail.com>
2015-02-15 11:04:02 -0500

    removes unused tasks

--- runner/config.js
+++ runner/config.js
@@ -1,8 +1,5 @@
 var src = '_src';
 var dest = '_dist';
-var bundleDest = dest + '/libs';
-var nodeModules = 'node_modules';
-var vDOM = './' + nodeModules + '/virtual-dom';
 
 module.exports = {
   src: src,
@@ -20,66 +17,9 @@ module.exports = {
     }
   },
 
-  copy: [
-    {
-      src: src + '/index.html',
-      dest: dest
-    },
-    {
-      src: 'node_modules/requirejs/require.js',
-      dest: dest
-    },
-    {
-      src: src + '/*.js',
-      dest: dest
-    }
-  ],
-
-  javascript: {
-    src: [
-      src + '/apps/**/*.js',
-      src + '/apps/**/*.jsx'
-    ],
-    dest: dest + '/apps',
-    watch: {
-      task: 'javascript'
-    }
-  },
-
-  jsx: {
-    src: [
-      src + '/apps/**/*.jsx'
-    ],
-    dest: dest + '/apps'
-  },
-
-  wrap: {
-    src: [
-      src + '/apps/**/*.js'
-    ],
-    dest: dest + '/apps',
-    wrapText: 'define(function(require){\n <%= contents %> \n});'
-  },
-
-  typescript: {
-    src: [
-      src + '/apps/app.ts',
-      src + '/apps/react/**/*.ts',
-      'lib.d/**'
-    ],
-    dest: dest,
-    opts: {
-      declarationFiles: false,
-      noExternalResolve: true,
-      sortOutput: false,
-      module: 'amd',
-      target: 'ES5'
-    }
-  },
-
   browser_sync: {
     server: {
-      baseDir: [ dest, nodeModules]
+      baseDir: [ dest]
     },
     files: [
       dest + '/**'
@@ -88,49 +28,6 @@ module.exports = {
 
   clean: {
     src: [ dest ]
-  },
-
-  browserify: {
-    // causes this task not to run
-    noop: true,
-    // A separate bundle will be generated for each item in the bundleConfigs array
-    bundleConfigs: [{
-      // csp library
-      entries: './' + nodeModules + '/js-csp/src/csp.js',
-      dest: bundleDest,
-      standalone: 'csp',
-      outputName: 'csp.js'
-    },{
-      // virtual-dom/h
-      entries: vDOM + '/h.js',
-      dest: bundleDest,
-      standalone: 'h',
-      outputName: 'h.js'
-    }, {
-      // virtual-dom/diff
-      entries: vDOM + '/diff.js',
-      dest: bundleDest,
-      standalone: 'diff',
-      outputName: 'diff.js'
-    }, {
-      // virtual-dom/patch
-      entries: vDOM + '/patch.js',
-      dest: bundleDest,
-      standalone: 'patch',
-      outputName: 'patch.js'
-    }, {
-      // virtual-dom/diff
-      entries: vDOM + '/create-element.js',
-      dest: bundleDest,
-      standalone: 'create-element',
-      outputName: 'create-element.js'
-    }, {
-      // dom-delegator
-      entries: './' + nodeModules + '/dom-delegator/dom-delegator.js',
-      dest: bundleDest,
-      standalone: 'dom-delegator',
-      outputName: 'dom-delegator.js'
-    }]
   }
 
 };
